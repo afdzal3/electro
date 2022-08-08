dd9720dc1b0f858e1330c4430580637243d85f27
Afdzal Kamal <afdzal3@gmail.com>
2022-08-08 15:45:19 +0800

    junit

--- src/test/java/electro/assignment/AssignmentApplicationTests.java
+++ src/test/java/electro/assignment/AssignmentApplicationTests.java
@@ -1,13 +1,42 @@
 package electro.assignment;
 
-import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
-class AssignmentApplicationTests {
+class AssignmentApplicationTests extends Application {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.
+                webAppContextSetup(webApplicationContext).build();
+    }
+
+
+    @Test
+    public void testAppliance() throws Exception {
+        mockMvc.perform(get("/api/status/list")).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+
+    }
+    
+        /**
+     * @Test void contextLoads() { }
+*
+     */
 
 }
